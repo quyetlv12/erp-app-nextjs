6d96b2a5eeb736d447e5548e457be62be1c8d3cc
quyetlv12 <lequyet840500@gmail.com>
2025-07-08 17:38:28 +0700

    add base

--- src/components/customers/modalDetailCustomer.tsx
+++ src/components/customers/modalDetailCustomer.tsx
@@ -30,7 +30,7 @@ const ModalDetailCustomer = ({
         <Dialog.Root open={open} onOpenChange={(open) => !open && onClose()}>
             <Dialog.Portal>
                 <Dialog.Overlay className="fixed inset-0 bg-black/30 z-[99998]" />
-                <Dialog.Content className="fixed top-1/2 left-1/2 z-[99999] w-full max-w-2xl bg-white p-0 rounded-lg shadow-lg transform -translate-x-1/2 -translate-y-1/2 h-[90vh] overflow-auto scroll-auto">
+                <Dialog.Content className="fixed top-1/2 left-1/2 z-[99999] w-full max-w-2xl bg-white p-0 rounded-lg shadow-lg transform -translate-x-1/2 -translate-y-1/2 h-fit overflow-auto scroll-auto">
                     <div className="relative p-0">
                         <div className="flex justify-between items-center bg-blue-50 p-5">
                             <div className="border-b border-gray-100 rounded-t-lg">
---
+++ src/models/Customer.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ICustomer extends Document {
+  code: string;
+  inCharge: string;
+  type: string;
+  name: string;
+  taxCode: string;
+  businessLicenseDate: string;
+  representative: string;
+  position: string;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+const CustomerSchema: Schema = new Schema({
+  code: { type: String, required: true },
+  inCharge: { type: String, required: true },
+  type: { type: String, required: true },
+  name: { type: String, required: true },
+  taxCode: { type: String, required: true },
+  businessLicenseDate: { type: String, required: true },
+  representative: { type: String, required: true },
+  position: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+export default mongoose.models.Customer ||
+  mongoose.model<ICustomer>("Customer", CustomerSchema);
